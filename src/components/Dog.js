e42306419e80e13c7abb68f50149792c53b6badb
olive <olivealavine@gmail.com>
2022-06-14 10:43:54 -0700

    added Dog and Doglist components to App

---
+++ src/components/Dog.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Dog = () => {
+  return (
+    <li>
+      <h3>Dog name</h3>
+      <h4>id</h4>
+      <h4>age</h4>
+      <h4>breed</h4>
+      <h4>chip</h4>
+    </li>
+  );
+};
+
+export default Dog;
---
+++ src/components/Doglist.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Dog from "./Dog.js";
+
+const DogList = () => {
+  return (
+    <ul>
+      <Dog />
+      <Dog />
+      <Dog />
+    </ul>
+  );
+};
+
+export default DogList;
